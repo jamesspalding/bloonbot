405c6e81f9d515043aa9a2b374caa8b32c552500
jamesspalding <jameshspalding01@gmail.com>
2024-10-23 12:50:29 -0500

    update main function

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import bloon_functions
 round = int(input("Enter starting round: "))
 pyautogui.hotkey('alt', 'tab')
 
-if __name__ == '__main__':
+def main():
     while True:
         if keyboard.is_pressed('q'):
             print('Ending...')
             break
         else:
 
-            time.sleep(2)
+            time.sleep(2) #time between checks
             if is_new_round():
                 print('Round ',round)
                 round = round+1
                 print(get_game_info())
                 pydirectinput.press('space')
             else:
-                print('Ongoing round') 
+                print('Ongoing round')  
+
+
+
+if __name__ == '__main__':
+    main()
